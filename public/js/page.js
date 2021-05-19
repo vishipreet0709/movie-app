c5f799dd35d35b14c44bb0412ee75a9ddc0af224
vishipreet <vishipreet0709@gmail.com>
2021-05-19 12:28:13 +0530

    v1

--- public/js/page.js
+++ public/js/page.js
@@ -3,6 +3,6 @@ const btn=document.querySelector('.btn')
 
 btn.addEventListener('click',(e)=>{
     const p=document.querySelector('.container').getAttribute('id');
-const q=document.querySelector('.container').getAttribute('data-q');
+const q=document.querySelector('.btn').getAttribute('id');
     window.location.href=`https://moviearena.herokuapp.com/cross?p=${p}&q=${q}`;
 })
